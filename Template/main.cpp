048b6cad2afc764dfbdb34c539a12900768c148b
lt <123>
2023-02-21 16:55:43 +0800

    Initial Commit

---
+++ Template/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Template
+//
+//  Created by 李笑微 on 2023/2/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
